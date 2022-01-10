7a96dcc7e60bbbc069153990606fa1e4b4eaacfd
Harminder Virk <virk.officials@gmail.com>
2022-01-10 11:26:54 +0530

    feat: initial implementation using chai.Assert

---
+++ src/Contracts/index.ts
@@ -0,0 +1,75 @@
+/*
+ * @japa/assert
+ *
+ * (c) Harminder Virk <virk@adonisjs.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+import { assert } from 'chai'
+
+/**
+ * Unnecessary similar methods have been removed
+ */
+export type ChaiAssert = { [K in keyof typeof assert]: typeof assert[K] }
+
+/**
+ * Assert contract
+ */
+export type AssertContract = Omit<
+  ChaiAssert,
+  | 'deepStrictEqual'
+  | 'nestedInclude'
+  | 'notNestedInclude'
+  | 'deepNestedInclude'
+  | 'notDeepNestedInclude'
+  | 'ifError'
+  | 'changes'
+  | 'changesBy'
+  | 'doesNotChange'
+  | 'changesButNotBy'
+  | 'increases'
+  | 'increasesBy'
+  | 'doesNotIncrease'
+  | 'increasesButNotBy'
+  | 'decreases'
+  | 'decreasesBy'
+  | 'doesNotDecrease'
+  | 'doesNotDecreaseBy'
+  | 'decreasesButNotBy'
+  | 'extensible'
+  | 'isExtensible'
+  | 'notExtensible'
+  | 'isNotExtensible'
+  | 'deepProperty'
+  | 'notDeepProperty'
+  | 'nestedProperty'
+  | 'nestedPropertyVal'
+  | 'notNestedProperty'
+  | 'notNestedPropertyVal'
+  | 'deepNestedProperty'
+  | 'notDeepNestedProperty'
+  | 'deepNestedPropertyVal'
+  | 'notDeepNestedPropertyVal'
+  | 'hasAnyKeys'
+  | 'hasAllKeys'
+  | 'containsAllKeys'
+  | 'doesNotHaveAnyKeys'
+  | 'doesNotHaveAllKeys'
+  | 'throw'
+  | 'Throw'
+  | 'doesNotThrow'
+  | 'hasAnyDeepKeys'
+  | 'hasAllDeepKeys'
+  | 'containsAllDeepKeys'
+  | 'doesNotHaveAnyDeepKeys'
+  | 'doesNotHaveAllDeepKeys'
+  | 'closeTo'
+  | 'operator'
+  | 'oneOf'
+  | 'ownInclude'
+  | 'notOwnInclude'
+  | 'deepOwnInclude'
+  | 'notDeepOwnInclude'
+>
---
+++ test-helpers/index.ts
@@ -0,0 +1,66 @@
+/*
+ * @japa/core
+ *
+ * (c) Harminder Virk <virk@adonisjs.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+import chai from 'chai'
+
+/**
+ * A modified copy of https://github.com/chaijs/chai/blob/a8359d3d15779a23a7957a4c52539d48de2763e0/test/bootstrap/index.js#L34
+ */
+export function expectError(fn: any, val?: any) {
+  try {
+    fn()
+  } catch (error) {
+    chai.expect(error).to.have.property('stack')
+
+    switch (chai.util['type'](val).toLowerCase()) {
+      case 'undefined':
+        return
+      case 'string':
+        return chai.expect(error.message).to.equal(val)
+      case 'regexp':
+        return chai.expect(error.message).to.match(val)
+      case 'object':
+        return Object.keys(val).forEach(function (key) {
+          chai.expect(error).to.have.property(key).and.to.deep.equal(val[key])
+        })
+    }
+
+    throw new chai.AssertionError('Invalid val')
+  }
+
+  throw new chai.AssertionError('Expected an error')
+}
+
+/**
+ * Same as exceptError, but for async functions
+ */
+export async function expectAsyncError(fn: any, val?: any) {
+  try {
+    await fn()
+  } catch (error) {
+    chai.expect(error).to.have.property('stack')
+
+    switch (chai.util['type'](val).toLowerCase()) {
+      case 'undefined':
+        return
+      case 'string':
+        return chai.expect(error.message).to.equal(val)
+      case 'regexp':
+        return chai.expect(error.message).to.match(val)
+      case 'object':
+        return Object.keys(val).forEach(function (key) {
+          chai.expect(error).to.have.property(key).and.to.deep.equal(val[key])
+        })
+    }
+
+    throw new chai.AssertionError('Invalid val')
+  }
+
+  throw new chai.AssertionError('Expected an error')
+}
